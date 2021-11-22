2c5ed27792bcb310d074a032fd9623c69db5f06a
daniel0426 <angus9209@gmail.com>
2021-11-22 15:12:51 +1300

    fetch movies and detail component complete

---
+++ src/apollo.js
@@ -0,0 +1,27 @@
+import {ApolloClient, InMemoryCache} from '@apollo/client';
+
+
+const client = new ApolloClient({
+    uri : 'https://movieql2.vercel.app/',
+    cache: new InMemoryCache(),
+    resolvers: {
+        Movie: {
+            isLiked: () => false
+        },
+        Mutation : {
+            toggleLikeMovie: (_, {id,isLiked}, {cache})=> {
+               cache.modify({
+                   id: `Movie:${id}`,
+                   fields: {
+                       isLiked: isLiked => !isLiked
+                   }
+               })
+                
+            },
+
+            
+        }
+    }
+})
+
+export default client;
